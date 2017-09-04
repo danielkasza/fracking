aacb2f5b09cc5b700008d847731abacac3d778e8
Daniel Kasza <daniel@kasza.hu>
2017-09-03 21:49:47 -0400

    Make previews work with facebook.
    Note that the URLs on the index page changed. The preview will only work with the new URLs, but the old URLs will continue working for everything else.

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ class Handler(BaseHTTPRequestHandler):
 
     # This works for everything except Julia sets.
     def do_GENERIC_FRACTAL(self, kernel, x_min):
-        tile_descriptor_string = self.path.split("/")[2]
+        tile_descriptor_string = self.path.split("/")[-1]
         (x, y, z) = [t(s) for t,s in zip((int,int,int), tile_descriptor_string.split("."))]
         width = 4.0/pow(2, min(z, 36))
         x = x_min + x*width
@@ -33,12 +33,12 @@ class Handler(BaseHTTPRequestHandler):
             self.do_STATIC("index.html", "text/html")
         elif self.path.startswith("/julia/") and not self.path.endswith(".jpg"):
             self.do_STATIC("julia.html", "text/html")
-        elif self.path == "/burningship":
+        elif self.path == "/burningship" or self.path == "/burningship/":
             self.do_STATIC("burningship.html", "text/html")
-        elif self.path == "/mandelbrot":
+        elif self.path == "/mandelbrot" or self.path == "/mandelbrot/":
             self.do_STATIC("mandelbrot.html", "text/html")
-        elif self.path == "/collatz":
-            self.do_STATIC("collatz.html", "text/html") 
+        elif self.path == "/collatz" or self.path == "/collatz/":
+            self.do_STATIC("collatz.html", "text/html")
         elif self.path == "/ol.js":
             self.do_STATIC("ol-4.1.1/ol.js", "application/javascript; charset=utf-8")
         elif self.path == "/ol.css":
@@ -47,7 +47,7 @@ class Handler(BaseHTTPRequestHandler):
             # Julia sets are a special case because the constant is also encoded in the URL.
             split = self.path.split("/")
             tile_descriptor_string1 = split[2]
-            tile_descriptor_string2 = split[3]
+            tile_descriptor_string2 = split[-1]
             (cr, ci) = [t(s) for t,s in zip((float,float), tile_descriptor_string1.split(","))]
             (x, y, z) = [t(s) for t,s in zip((int,int,int), tile_descriptor_string2.split("."))]
             width = 4.0/pow(2, min(z, 36))
@@ -71,7 +71,7 @@ class Handler(BaseHTTPRequestHandler):
             self.send_header('Content-type', "text/html")
             self.end_headers()
             self.wfile.write("<h1>404 - Not Found</h1>\n")
-            
+
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """"""
